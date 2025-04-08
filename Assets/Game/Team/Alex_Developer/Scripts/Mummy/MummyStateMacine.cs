8f49076a9ecd250ae2bade0dcc3ea36630275fdf
Alexeiad <103834584+Alexeiad@users.noreply.github.com>
2025-04-08 23:10:42 +0300

    Create trigger zone

--- Assets/Game/Team/Alex_Developer/Scripts/Mummy/MummyStateMacine.cs
+++ Assets/Game/Team/Alex_Developer/Scripts/Mummy/MummyStateMacine.cs
@@ -1,42 +1,44 @@
+using Unity.IO.LowLevel.Unsafe;
+using Unity.VisualScripting;
 using UnityEngine;
+
 
 [RequireComponent(typeof(CharacterController))]
 public class MummyStateMachine : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
+    [SerializeField] private Light _spotLight;
     [SerializeField] private float _walkSpeed = 5f;
     [SerializeField] private float _runSpeed = 8f;
     [SerializeField] private float _rotationSpeed = 10f;
     [SerializeField] private float _detectionDistance = 4f;
     [SerializeField] private float _viewAngle = 120f;
-
+    
     private CharacterController _mummyController;
-    private StateMachine _stateMachine; 
-
-    private Transform _player;
+    private StateMachine _stateMachine;
 
     private bool _isIdle;
     private bool _isPatrolling;
 
+    State idleState;
+    State patrollingState;
+
     private void Awake()
     {
         _mummyController = GetComponent<CharacterController>();
-        _player = FindFirstObjectByType<PlayerCapsule>().transform;
-
+        
         InitializeStateMachine();
     }
 
     private void Update()
     {
         _stateMachine.OnUpdate();
-
-        DetectPlayer();
     }
 
     private void InitializeStateMachine()
     {
-        State idleState = new MummyIdleState(_animator, _mummyController);
-        State patrollingState = new MummyPatrollingState(_animator, _mummyController);
+        idleState = new MummyIdleState(_animator, _mummyController);
+        patrollingState = new MummyPatrollingState(_animator, _mummyController);
 
         idleState.AddTransition(new StateTransition(patrollingState, new FuncStateCondition(() => _isPatrolling))); // Заменено на логику с лучом
         patrollingState.AddTransition(new StateTransition(idleState, new FuncStateCondition(() => _isIdle))); // Заменено на логику с лучом
@@ -44,29 +46,26 @@ public class MummyStateMachine : MonoBehaviour
         _stateMachine = new StateMachine(idleState);
     }
 
-    private void DetectPlayer()
+    private void OnTriggerEnter(Collider other)
     {
-        if (_player != null)
+        if (other.GetComponent<PlayerCapsule>())
         {
-            Vector3 directionToPlayer = (_player.position - transform.position).normalized;
+            _isPatrolling = false;
+            _isIdle = true;
+            _stateMachine = new StateMachine(patrollingState);
+        }
+    }
 
-            float angle = Vector3.Angle(transform.position, directionToPlayer);
-
-            if (angle < _viewAngle * 0.5f) 
-            {
-                if (Physics.Raycast(transform.position, directionToPlayer, out RaycastHit hit, _detectionDistance))
-                {
-                    _isPatrolling = true;
-                    _isIdle = false;
-                }
-                
-            }
-            else
-            {
-                _isPatrolling = false;
-                _isIdle = true;
-            }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<PlayerCapsule>())
+        {
+            _isPatrolling = false;
+            _isIdle = true;
+            _stateMachine = new StateMachine(idleState);
         }
     }
 }
+        
+    
     
