d97ece3464089466d2bcfef2e6be8f3444d981cf
Alexeiad <103834584+Alexeiad@users.noreply.github.com>
2025-04-23 22:01:51 +0300

    -Add Inventary items

---
+++ Assets/Game/Team/Alex_Developer/Scripts/Inventary/Inventary.cs
@@ -0,0 +1,36 @@
+
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
+using UnityEngine;
+using UnityEngine.Localization.SmartFormat.Core.Parsing;
+
+public class Inventary:MonoBehaviour 
+{ 
+    [SerializeField] private List<RectTransform> _slots;
+    [SerializeField] private RectTransform _selector;
+    [SerializeField] private float _lerpSpeed = 5f;
+
+    private int currentIndex;
+
+    private void Update()
+    {
+        float scrollInput = Input.mouseScrollDelta.y;
+
+        if (_slots.Count == 0)
+            return;
+
+        currentIndex += (int)scrollInput;
+
+        if (currentIndex >= _slots.Count)
+            currentIndex = 0;
+        else if (currentIndex < 0)
+            currentIndex = _slots.Count - 1;
+
+        Vector2 targetPosition = _slots[currentIndex].anchoredPosition;
+
+        _selector.anchoredPosition = currentIndex > 0 && currentIndex < _slots.Count-1 ? Vector2.Lerp(_selector.anchoredPosition,
+            targetPosition, _lerpSpeed * Time.deltaTime) : _slots[currentIndex].anchoredPosition;
+    }
+
+}
